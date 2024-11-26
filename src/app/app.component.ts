7d2f554c904c18410687a703efd2b418ed6e4c51
jassu7093 <jaswanthpoondru@gmail.com>
2024-11-26 12:52:10 +0530

    commit change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,AboutComponent,LoginComponent,SignupComponent,ContactComponent,ServicesComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",component:HomeComponent
+        },
+    {
+    path:"about",component:AboutComponent
+    },
+    {
+    path:"login",component:LoginComponent
+    },
+    {
+        path:"signup",component:SignupComponent
+    },
+    {
+        path:"contact",component:ContactComponent
+    },
+    {
+        path:"services",component:ServicesComponent
+    }
+];
